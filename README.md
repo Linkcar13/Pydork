98b08bcbc6311e69cd87abeb5e30fd8f7125f1af
Linkcar13 <carlos.estrada01@epn.edu.ec>
2024-06-05 14:47:43 -0500

    add: new functions

--- README.md
+++ README.md
@@ -7,6 +7,28 @@ This tool is just for educational purposes, any other use is absolutely your res
 ## Demo
 ![pydork demo](pydork_ex.png)
 
+## Installation
+
+To get this project up and running on your local machine, follow these steps:
+
+Clone the repository
+
+Open your terminal and run the following command to clone the repository:
+
+```bash
+git clone https://github.com/AEIS-FIS-EPN/carlos-estrada-portfolio.git
+```
+
+Navigate to the project directory, After cloning, enter the project directory by running:
+
+```bash
+cd Pydork
+```
+
+Install the necessary dependencies by running:
+```bash
+pip install -r requirements.txt
+```
 
 ## Usage
 ```bash
--- pydork.py
+++ pydork.py
@@ -1,8 +1,12 @@
+import argparse
 import requests
 import time
 import sys
 import re
 from bs4 import BeautifulSoup
+import json
+import markdown ## For reporting
+from prettytable import PrettyTable
 
 # Definición de Colores
 
@@ -23,24 +27,25 @@ white="\033[0;37m"
 nc="\033[00m"
 
 # Banner
-banner = f'''
-{bblue}██████╗ ██╗   ██╗██████╗  ██████╗ ██████╗ ██╗  ██╗    
-{bblue}██╔══██╗╚██╗ ██╔╝██╔══██╗██╔═══██╗██╔══██╗██║ ██╔╝    
-{yellow}██████╔╝ ╚████╔╝ ██║  ██║██║   ██║██████╔╝█████╔╝     
-{green}██╔═══╝   ╚██╔╝  ██║  ██║██║   ██║██╔══██╗██╔═██╗     
-{red}██║        ██║   ██████╔╝╚██████╔╝██║  ██║██║  ██╗    
-{red}╚═╝        ╚═╝   ╚═════╝  ╚═════╝ ╚═╝  ╚═╝╚═╝  ╚═╝    
-{purple}                                by [KalelS] v1.1{nc}
-'''
+def print_banner():
+    banner = f'''
+    {bblue}██████╗ ██╗   ██╗██████╗  ██████╗ ██████╗ ██╗  ██╗    
+    {bblue}██╔══██╗╚██╗ ██╔╝██╔══██╗██╔═══██╗██╔══██╗██║ ██╔╝    
+    {yellow}██████╔╝ ╚████╔╝ ██║  ██║██║   ██║██████╔╝█████╔╝     
+    {green}██╔═══╝   ╚██╔╝  ██║  ██║██║   ██║██╔══██╗██╔═██╗     
+    {red}██║        ██║   ██████╔╝╚██████╔╝██║  ██║██║  ██╗    
+    {red}╚═╝        ╚═╝   ╚═════╝  ╚═════╝ ╚═╝  ╚═╝╚═╝  ╚═╝    
+    {purple}                                by [KalelS] v1.2{nc}
+    '''
 
-print(banner)
+    print(banner)
 
 
-if len(sys.argv) != 2:
+""" if len(sys.argv) != 2:
     print(f'''{red}[!] Usage: python google_dork.py <domain>''')
-    sys.exit(1)
+    sys.exit(1) """
 
-# Diccionario de dorks más comunes
+""" # Diccionario de dorks más comunes
 dorks = [
     ##
     "site:{} ext:doc".format(sys.argv[1]),
@@ -52,6 +57,7 @@ dorks = [
     "site:{} ext:log".format(sys.argv[1]),
     "site:{} ext:sql".format(sys.argv[1]),
     "site:{} ext:pdf".format(sys.argv[1]),
+     "site:{} ext:txt".format(sys.argv[1]),
     ##
     "site:{} filetype:doc".format(sys.argv[1]),
     "site:{} filetype:docx".format(sys.argv[1]),
@@ -62,10 +68,11 @@ dorks = [
     "site:{} filetype:log".format(sys.argv[1]),
     "site:{} filetype:sql".format(sys.argv[1]),
     "site:{} filetype:pdf".format(sys.argv[1]),
+    "site:{} filetype:txt".format(sys.argv[1]),
     ##
     "site:{} inurl:admin intext:password".format(sys.argv[1]),
     "site:{} intitle:index of".format(sys.argv[1]),
-    #"site:{} ext:doc | ext:pdf | ext:docx | ext:txt | ext:xlsx | ext:xls".format(sys.argv[1]),
+    "site:{} ext:doc | ext:pdf | ext:docx | ext:txt | ext:xlsx | ext:xls".format(sys.argv[1]),
     "site:{} inurl:config intext:password".format(sys.argv[1]),
     "site:{} ext:log".format(sys.argv[1]),
     "site:{} ext:sql".format(sys.argv[1]),
@@ -82,6 +89,9 @@ dorks = [
     "inurl:{} ext:ppt".format(sys.argv[1]),
     "inurl:{} ext:pptx".format(sys.argv[1]),
     "inurl:{} ext:pdf".format(sys.argv[1]),
+    "inurl:{} ext:txt".format(sys.argv[1]),
+    "inurl:{} ext:sql".format(sys.argv[1]),
+    "inurl:{} ext:log".format(sys.argv[1]),
     "related:{} ext:doc".format(sys.argv[1]),
     "related:{} ext:docx".format(sys.argv[1]),
     "related:{} ext:xls".format(sys.argv[1]),
@@ -89,6 +99,9 @@ dorks = [
     "related:{} ext:ppt".format(sys.argv[1]),
     "related:{} ext:pptx".format(sys.argv[1]),
     "related:{} ext:pdf".format(sys.argv[1]),
+    "related:{} ext:txt".format(sys.argv[1]),
+    "related:{} ext:sql".format(sys.argv[1]),
+    "related:{} ext:log".format(sys.argv[1]),
     ##
     "inurl:{} filetype:doc".format(sys.argv[1]),
     "inurl:{} filetype:docx".format(sys.argv[1]),
@@ -97,6 +110,15 @@ dorks = [
     "inurl:{} filetype:ppt".format(sys.argv[1]),
     "inurl:{} filetype:pptx".format(sys.argv[1]),
     "inurl:{} filetype:pdf".format(sys.argv[1]),
+    "inurl:{} filetype:txt".format(sys.argv[1]),
+    "inurl:{} filetype:log".format(sys.argv[1]),
+    "inurl:{} .env".format(sys.argv[1]),
+    "inurl:{} filetype:sql".format(sys.argv[1]),
+    "inurl:{} config".format(sys.argv[1]),
+    "inurl:{} user".format(sys.argv[1]),
+    "inurl:{} usuario".format(sys.argv[1]),
+    "inurl:{} contraseña".format(sys.argv[1]),
+    "inurl:{} password".format(sys.argv[1]),
     "related:{} filetype:doc".format(sys.argv[1]),
     "related:{} filetype:docx".format(sys.argv[1]),
     "related:{} filetype:xls".format(sys.argv[1]),
@@ -104,45 +126,132 @@ dorks = [
     "related:{} filetype:ppt".format(sys.argv[1]),
     "related:{} filetype:pptx".format(sys.argv[1]),
     "related:{} filetype:pdf".format(sys.argv[1]),
-]
+    "related:{} filetype:txt".format(sys.argv[1]),
+    "related:{} filetype:sql".format(sys.argv[1]),
+    "related:{} filetype:log".format(sys.argv[1]),
+    "related:{} .env".format(sys.argv[1]),
+    "related:{} config".format(sys.argv[1]),
+    "related:{} username".format(sys.argv[1]),
+    "related:{} contraseña".format(sys.argv[1]),
+    "related:{} user".format(sys.argv[1]),
+    "related:{} password".format(sys.argv[1]),
+] """
 
-# Delay apra evitar los bloqueos
-delay = 10
 
-# Se recorre el diccionario de dorks
-for dork in dorks:
-    # Se establece la query
-    query = f"https://www.google.com/search?q={dork}"
+    ## Define domain for search
+    #domain = sys.argv[1]
+def read_file(file):
+    #Custom file of dorks
+    try:
+        with open(file,'r') as json_file:
+            decoded_file = json.load(json_file)
+    except FileNotFoundError:
+        print(f"File {file} not found!!")
+        sys.exit(1)
+    except json.JSONDecodeError:
+        print(f"decode of JSON file {file} failed")
+        sys.exit(1)
+    return decoded_file
+
+def search(domain, file_dorks, file_filters,output):        
+    # Delay para evitar los bloqueos
+    delay = 10
+    relevant_results = dict()
+    results = dict()
+    dorks = read_file(file_dorks)
+    filters = read_file(file_filters)
+    # Se recorre el diccionario de dorks
+    for dork in dorks:
+        # Se establece la query
+        query = f"https://www.google.com/search?q={dork.format(domain)}"
+        response = requests.get(query)
+        #print(response.text)
+        if response.status_code == 200:
+            print(f'''{blue}[+] Resultados para: '{dork.format(domain)}':\n''')
+            soup = BeautifulSoup(response.text, "html.parser")
+
+            #results = soup.find_all("div")
+            #for result in results:
+            urls = soup.find_all("div")
+            for result in soup.find_all("div"):
+                link = result.find("a")
+                title = result.find("h3")
+                if link and title:
+                    title = result.find("h3").get_text()
+                    url = link.get("href")
+                    urlf = re.search(r'/url\?q=(.*?)&', url)
+                    if urlf:
+                        url = urlf.group(1)
+                    print(f'''{bgreen}[!!] Resultado encontrado:\n''')
+                    print(f'''{nc}Title: {title}''')
+                    print(f'''{nc}Link: {url}\n''')
+                    results["dork"] = dork
+                    results["Title"] = title
+                    results["Link"] = url
+
+                    for interesting in filters:
+                        if str(interesting) in title or str(interesting) in link:
+                            relevant_results["Title"] = title
+                            relevant_results["Link"] = url
+                    
+                # else:
+                #     print(f"[!!] Resultado encontrado sin enlace:")
+                #     #print(f"Title: {title}\n")
+                #     #print(f"Link: {url}\n")       
+        else:
+            print(f'''{red}\n [-] Error al econtrar resultados para: '{dork}''')
+        time.sleep(delay)
+    gen_report(len(dorks),len(results["dork"]),relevant_results,results)
+
+def gen_report(num_dorks,num_results,relevant_results,total_results):
+    
+    print_banner()
+    relevant = PrettyTable()
+    max_length = max(len(column) for column in relevant.values())
+
+    
+    for key in relevant:
+        if len(relevant[key]) < max_length:
+            relevant[key] += [''] * (max_length - len(relevant[key]))
+
+    for key, value in relevant_results.items():
+        relevant.add_column(key, value)
+    print("Interesting Results:")
+    print("------------------------------------------------------")
+    print(relevant)
 
 
-    response = requests.get(query)
+    results = PrettyTable()
 
-    #print(response.text)
+    max_length2 = max(len(column) for column in results.values())
 
-    if response.status_code == 200:
-        print(f'''{blue}[+] Resultados para: '{dork}':\n''')
-        soup = BeautifulSoup(response.text, "html.parser")
+    
+    for key in results:
+        if len(results[key]) < max_length2:
+            results[key] += [''] * (max_length2 - len(results[key]))
 
-        #results = soup.find_all("div")
-        #for result in results:
-        urls = soup.find_all("div")
-        for result in soup.find_all("div"):
-            link = result.find("a")
-            title = result.find("h3")
-            if link and title:
-                title = result.find("h3").get_text()
-                url = link.get("href")
-                urlf = re.search(r'/url\?q=(.*?)&', url)
-                if urlf:
-                    url = urlf.group(1)
-                print(f'''{bgreen}[!!] Resultado encontrado:\n''')
-                print(f'''{nc}Title: {title}''')
-                print(f'''{nc}Link: {url}\n''')
-            # else:
-            #     print(f"[!!] Resultado encontrado sin enlace:")
-            #     #print(f"Title: {title}\n")
-            #     #print(f"Link: {url}\n")       
-    else:
-        print(f'''{pink}\n [-] Error al econtrar resultados para: '{dork}''')
-    time.sleep(delay)
 
+    for key, value in total_results.items():
+        results.add_column(key, value)
+    print("Results:")
+    print("------------------------------------------------------")
+    print(results)
+
+    print(f"dorks executed: {num_dorks}")
+    print(f"total of results: {num_dorks}")
+
+    print("ReportGenrated")
+
+
+if __name__ == "__main__":
+    print_banner()
+
+    parser = argparse.ArgumentParser(description="Automated Google Doorking Tool")
+    parser.add_argument("--domain", required=True, help="Domain for Google Dorking")
+    parser.add_argument("--file_dorks", default="dorks.json", help="Path to custom dorks dictionary")
+    parser.add_argument("--file_filters", default="filters.json", help="Path to custom filters for results dictionary")
+    parser.add_argument("--output", default="report.md", help="Path to save md report")
+
+    args = parser.parse_args()
+
+    search(args.domain, args.file_dorks, args.file_filters, args.output)
